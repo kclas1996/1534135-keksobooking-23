f5e0aab21a656d52a7ce44de33bf90a1d4bc911b
Ksenia <kclas.tennis@gmail.com>
2021-06-12 23:10:54 +0300

    Добавила изменения

--- js/main.js
+++ js/main.js
@@ -2,24 +2,23 @@
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  if((min<=max) && (min,max>=0)) {
+  if((min<=max) && (min,max>=0) && (min % 1 === 0) && (max % 1 === 0)) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  } else {
+    throw new Error('В качестве аргументов используются только целые числа');
+  }
+}
+
+//console.log(getRandomIntInclusive(1,2));
+
+
+function getRandomFloat(min, max, n) {
+  if((min<=max) && (min,max>=0)) {
+    return (min + (Math.random() * (max - min))).toFixed(n);
   } else {
     return('Неверный диапазон чисел');
   }
 }
-
-getRandomIntInclusive(1,20);
-
-
-function getRandomFloat(min, max,) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  if((min<=max) && (min,max>=0)) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  } else {
-    return('Неверный диапазон чисел');
-  }
-}
-getRandomFloat.toFixed((11, 101).toFixed(2));
+console.log(getRandomFloat(1.2353474848, 2.4787559, 3));
+ 
 
